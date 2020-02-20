e50ce3953c3400d16a85c9b87d7f17d2cbfbc817
Lacra <lacramioara.oloeriu@gmail.com>
2020-02-20 14:31:14 +0100

    Modifid the Tweet component to ajust the const tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,7 +4,13 @@ import { formatTweet } from '../utils/helpers';
 
 export class Tweet extends Component {
     render() {
-        console.log(this.props);
+        const { tweet } = this.props
+
+        if (tweet === null) {
+            return <p> This Tweet doesn't existd </p>
+        }
+
+        console.log(this.props)
         return (
             <div className="tweet">
                 <h3>Tweet</h3>
@@ -15,10 +21,12 @@ export class Tweet extends Component {
 
 function mapStateToProps({ authedUser, users, tweets }, { id }) {
     const tweet = tweets[id];
+    const parentTweet = tweet ? tweets[tweet.replaingTo] : null
 
     return {
         authedUser,
-        tweet: formatTweet(tweet, users[tweet.author], authedUser)
+        tweet: tweet ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet) :
+            null
     };
 }
 export default connect(mapStateToProps)(Tweet);
